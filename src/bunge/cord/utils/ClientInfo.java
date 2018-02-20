040d46c1bb046c2dc8a98afb18ab82c16cefda50
RedstoneAlmeida <mineps3marcos@gmail.com>
2018-02-20 14:43:10 -0300

    Added ClientInfo

---
+++ src/bunge/cord/utils/ClientInfo.java
@@ -0,0 +1,20 @@
+package bunge.cord.utils;
+
+public class ClientInfo {
+
+    public String name;
+    public long serverId;
+    public int slots;
+
+    public String getName() {
+        return name;
+    }
+
+    public long getServerId() {
+        return serverId;
+    }
+
+    public int getSlots() {
+        return slots;
+    }
+}
